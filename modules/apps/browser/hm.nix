dd0e0f676f4404b4454825b77670ace41865133d
CypherWhisperer <cypherwhisperer@gmail.com>
2026-04-20 10:24:02 +0300

    update: minor changes to the file to clean it up

--- modules/apps/browser/hm.nix
+++ modules/apps/browser/hm.nix
@@ -1,16 +1,12 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 
 {
-  config = lib.mkIf (
-    config.cypher-os.apps.enable &&
-    config.cypher-os.apps.browser.enable ) {
-
-      cypher-os.apps.browser.brave.enable = lib.mkDefault true;
-      cypher-os.apps.browser.firefox.enable = lib.mkDefault true;
-    };
+  config = lib.mkIf (config.cypher-os.apps.enable && config.cypher-os.apps.browser.enable) {
+    cypher-os.apps.browser.brave.enable = lib.mkDefault true;
+    cypher-os.apps.browser.firefox.enable = lib.mkDefault true;
+  };
 }
