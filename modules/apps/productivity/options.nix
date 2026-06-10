2d854c671948af922068c4f1cf880d35e456a81d
CypherWhisperer <cypherwhisperer@gmail.com>
2026-06-10 12:06:04 +0300

    update: add support for penpot as a toggle-able option

--- modules/apps/productivity/options.nix
+++ modules/apps/productivity/options.nix
@@ -5,5 +5,6 @@
     enable = lib.mkEnableOption "CypherOS Productivity Applications";
     claude.enable = lib.mkEnableOption "Claude Desktop";
     obsidian.enable = lib.mkEnableOption "Obsidian Desktop App";
+    penpot.enable = lib.mkEnableOption "Penpot Design App";
   };
 }
