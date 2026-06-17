96da60fe470f986b413b30608838a50b86c9b505
CypherWhisperer <cypherwhisperer@gmail.com>
2026-06-17 15:24:49 +0300

    update: add SAL_USE_VCLPLUGIN environment variable and set it to gtk3 for libreOffice to respect GTK3 theming

--- modules/apps/productivity/libreoffice.nix
+++ modules/apps/productivity/libreoffice.nix
@@ -9,8 +9,10 @@ let
   ctpLO = pkgs.fetchurl {
     # Catppuccin Mocha Mauve .soc
     url = "https://github.com/catppuccin/libreoffice/raw/main/themes/mocha/mauve/catppuccin-mocha-mauve.soc";
-    sha256 = lib.fakeHash;
+    sha256 = "sha256-c7BIwKlwUpD+rLKQQi43mHi2s/hlNkxPE+eX7iWb2vI=";
   };
+
+in
 
 {
   config =
@@ -23,5 +25,13 @@ let
         ];
 
         home.file.".config/libreoffice/4/user/config/catppuccin-mocha-mauve.soc".source = ctpLO;
-  };
+
+        # LibreOffice on NixOS frequently needs `SAL_USE_VCLPLUGIN=gtk3` set
+        # explicitly as an environment variable, otherwise it falls back to its own
+        # VCL widget system. Settin the env var forces LibreOffice to use the GTK3
+        # rendering backend, at which point it will respect your system GTK3 theme.
+        home.sessionVariables = {
+          SAL_USE_VCLPLUGIN = "gtk3";
+        };
+      };
 }
