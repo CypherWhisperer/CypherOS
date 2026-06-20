cb2283849fe74b1c3331db84662149a4ce8fe34b
CypherWhisperer <cypherwhisperer@gmail.com>
2026-06-20 17:07:36 +0300

    update: minor update to tor-system.nix file

--- modules/apps/browser/tor-system.nix
+++ modules/apps/browser/tor-system.nix
@@ -3,7 +3,6 @@
 # Tor Daemon module for CypherOS.
 {
   config,
-  pkgs,
   lib,
   ...
 }:
@@ -11,31 +10,34 @@
 {
   imports = [ ./options.nix ];
 
-  config = lib.mkIf (config.cypher-os.apps.browser.enable && config.cypher-os.apps.browser.tor.enable) {
-  # ── System-level tor daemon (optional but recommended) ────────────────────
-  # Enabling this  gives you:
-  #   - Persistent tor circuits across Tor Browser launches
-  #
-  #   - A SOCKS5 proxy at 127.0.0.1:9050 usable by other tools:
-  #       torsocks curl https://check.torproject.org/api/ip
-  #       ssh -o ProxyCommand='ncat --proxy 127.0.0.1:9050 --proxy-type socks5 %h %p' user@host.onion
-  #
-  #   - tor control port at 127.0.0.1:9051 for advanced circuit management
-  #
-  # Specific use case (OnionShare, torsocks, etc.)
-  services.tor = {
-    enable = true;
-    client.enable = true; # SOCKS5 proxy at :9050
-    settings = {
-      # Increase guard node rotation time (reduces correlation window)
-      GuardLifetime = "60 days";
+  config =
+    lib.mkIf (config.cypher-os.apps.browser.enable && config.cypher-os.apps.browser.tor.enable)
+      {
+        # ── System-level tor daemon (optional but recommended) ────────────────────
+        # Enabling this  gives you:
+        #   - Persistent tor circuits across Tor Browser launches
+        #
+        #   - A SOCKS5 proxy at 127.0.0.1:9050 usable by other tools:
+        #       torsocks curl https://check.torproject.org/api/ip
+        #       ssh -o ProxyCommand='ncat --proxy 127.0.0.1:9050 --proxy-type socks5 %h %p' user@host.onion
+        #
+        #   - tor control port at 127.0.0.1:9051 for advanced circuit management
+        #
+        # Specific use case (OnionShare, torsocks, etc.)
+        services.tor = {
+          enable = true;
+          client.enable = true; # SOCKS5 proxy at :9050
+          settings = {
+            # Increase guard node rotation time (reduces correlation window)
+            GuardLifetime = "60 days";
 
-      # Enforce only high-bandwidth, stable exit nodes
-      ExitNodes = "{us},{de},{nl},{ch}"; # optional; restrict exit jurisdictions
+            # Enforce only high-bandwidth, stable exit nodes
+            ExitNodes = "{us},{de},{nl},{ch}"; # optional; restrict exit jurisdictions
 
-      # Stream isolation: separate circuit per destination
-      IsolateDestAddr = 1;
-      IsolateDestPort = 1;
-    };
-  };
+            # Stream isolation: separate circuit per destination
+            IsolateDestAddr = 1;
+            IsolateDestPort = 1;
+          };
+        };
+      };
 }
